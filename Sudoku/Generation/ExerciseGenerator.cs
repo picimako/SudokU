ef310513ac914ff1728f072e22e54e88a96ae743
Tamas Balog <picimako@gmail.com>
2016-05-23 21:51:40 +0200

    Rename method and variables to English and more meaningful names

--- Sudoku/Generation/ExerciseGenerator.cs
+++ Sudoku/Generation/ExerciseGenerator.cs
@@ -62,12 +62,11 @@ namespace Sudoku.Generate
                 {
                     se.InitKillerExercise();
 
-                    //A megoldást tároló tömbbe átmásolom a megoldást
                     se.Killer.Ctrl.CopySolutionToExercise(se.Exercise[0]);
 
                     /* Legenerálok egy felosztást.
                      * Ha van nem elhelyezhető cella, akkor új felosztást készítek*/
-                } while (!killerGeneralas());
+                } while (!GenerateKiller());
 
                 //Kitörlök minden értéket a feladatból, mert üres táblát kell megadni a feladatban
                 int[][,] tombok;
@@ -77,91 +76,91 @@ namespace Sudoku.Generate
                 //81 lesz az üres cellák száma, mert nem kell megadni egyetlen egy számot sem a feladatban
                 se.NumberOfEmptyCells = se.LAST_CELL_INDEX;
             }
-            //Ha nem Killer
             else
             {
                 if (util.Difficulty != 0)
                     remover.RemoveNumbersWithBackTrack(); //További számok kivétele nehezítés gyanánt
             }
 
-            //A kimenő paraméter megkapja a feladat megoldását
             if (!se.IsExerciseKiller)
             {
                 se.Solution = util.Solution[0];
             }
         }
 
-        /// <summary> Elkészít egy Killer felosztást. </summary>
-        public bool killerGeneralas()
+        /// <summary>Generates a Killer layout on an already generated Sudoku solution.</summary>
+        public bool GenerateKiller()
         {
-            /* kSzam: az aktuális ketrec sorszáma
-             * aktKetrecMeret: az aktuális ketrec elemszáma
-             * p: számolja, hogy az aktuális ketrecbe hány számot raktam már be*/
-            int kSzam = 1, aktKetrecMeret, p;
+            int cageIndex = 1;
+            int currentCageSize;
+            int numberOfCellsPutInCurrentCage;
             
-            //Egy cella lehetséges szomszédai indexeinek tárolására
-            List<Cell> szomszedok = new List<Cell>();
-            //Egy egyedül maradt cella, lehetséges szomszéd ketreceinek tárolására
-            List<int> szomszedKetrecek = new List<int>();
+            List<Cell> neighbourCells = new List<Cell>();
+            //Egy egyedül maradt cella lehetséges szomszéd ketreceinek tárolására
+            List<int> neighbourCages = new List<int>();
             //Az aktuálisan feldolgozandó cella indexeit tárolja
-            Cell aktCella = new Cell(0, 0);
+            Cell currentCell = new Cell(0, 0);
 
             do
             {
-                p = 0;
+                numberOfCellsPutInCurrentCage = 0;
 
                 /* Az aktuális ketrec mérete (benne levő cellák száma) a [2, killerNehezseg + 3) halmazból egy szám
                  * killerNehezseg + 3 - 1 az a szám, amekkora legnagyobb méretű ketreceket megengedek
                  * Persze ettől függetlenül létrejöhetnek olyan ketrecek, amelyek ennél több cellából állnak, mert általában van olyan cella,
                  * ami önmaga alkotna egy ketrecet, de ekkor hozzá kell venni egy szomszéd cella ketrecéhez.*/
-                aktKetrecMeret = random.Next(2, util.KillerDifficulty + 3);
+                currentCageSize = random.Next(2, util.KillerDifficulty + 3);
 
-                //Az aktuális cellát elhelyezem az aktuális ketrecben
-                se.Killer.Ctrl.PutCellInCage(aktCella, kSzam);
+                se.Killer.Ctrl.PutCellInCage(currentCell, cageIndex);
 
                 do
                 {
                     /* Lekérem az aktuális cella minden szabad szomszédját.
                      * (ami még nem szerepel egyik ketrecben sem és a ketrec nem tartalmazza az aktuális cella értékét.*/
-                    if ((szomszedok = se.Killer.Ctrl.FindPossibleNeighbourCells(aktCella, kSzam, true)).Count != 0)
+                    neighbourCells = se.Killer.Ctrl.FindPossibleNeighbourCells(currentCell, cageIndex, true);
+                    if (neighbourCells.Count != 0)
                     {
                         //Ha van lehetséges szomszéd, akkor véletlenszerűen választok egyet közülük
-                        aktCella = szomszedok[random.Next(0, szomszedok.Count)];
+                        currentCell = neighbourCells[RandomFrom(neighbourCells.Count)];
 
                         //A kiválasztott szomszédot elhelyezem a ketrecben
-                        se.Killer.Ctrl.PutCellInCage(aktCella, kSzam);
+                        se.Killer.Ctrl.PutCellInCage(currentCell, cageIndex);
                     }
                     else
                     {
                         /* Ha pedig nincs lehetséges szomszédja, akkor ezt a cellát belle kell raknom valamelyik olyan szomszéd ketrecébe, ahol 9-nél
                          * kevesebb elem van, és az aktuális cella értéke még nem szerepel abban a ketrecben.
                          * Ha az aktuális ketrecben meg nincs cella*/
-                        if (p == 0)
+                        if (numberOfCellsPutInCurrentCage == 0)
                         {
                             //Ha nincs lehetséges választható szomszéd ketrec, 
-                            if ((szomszedKetrecek = se.Killer.Ctrl.FindPossibleNeighbourCages(aktCella)).Count == 0)
+                            if ((neighbourCages = se.Killer.Ctrl.FindPossibleNeighbourCages(currentCell)).Count == 0)
                                 return false; //visszatérek false-szal, és a felosztás készítése elölről kezdődik
 
                             /* Ha van választható szomszéd ketrec, akkor választok közülük
                              * Az aktuális (egyedül maradt) cellát elhelyezem a kiválasztott szomszéd ketrecben*/
-                            se.Killer.Ctrl.PutCellInCage(aktCella, szomszedKetrecek[random.Next(0, szomszedKetrecek.Count)]);
+                            se.Killer.Ctrl.PutCellInCage(currentCell, neighbourCages[RandomFrom(neighbourCages.Count)]);
 
                             //Kitörlöm a cella előző ketrecszámához tartozó értéket a ketreceket tároló szótárból
-                            se.Killer.Cages.Remove(kSzam);
+                            se.Killer.Cages.Remove(cageIndex);
                         }
 
                         //Elkezdek egy új ketrecet létrehozni
                         break;
                     }
                     //Addig megy, amíg a ketrec elemszámának megfelelő cellát el nem helyezek a ketrecben
-                } while (++p < aktKetrecMeret);
-                //Növelem a ketrec sorszámát
-                kSzam++;
+                } while (++numberOfCellsPutInCurrentCage < currentCageSize);
+                cageIndex++;
                 /* Megkeresem a legelső 0 ketrecértékű cellát, és a megtalált cella indexeit kapja értékül aktCella.
                  * Ha az indexek értéke -1, akkor teli van a tábla, tehát kész a felosztás.*/
-            } while ((aktCella = se.Killer.Ctrl.FindFirstEmptyCell()).Row != -1);
+            } while ((currentCell = se.Killer.Ctrl.FindFirstEmptyCell()).Row != -1);
 
             return true;
+        }
+
+        private int RandomFrom(int number)
+        {
+            return random.Next(0, number);
         }
     }
 }
