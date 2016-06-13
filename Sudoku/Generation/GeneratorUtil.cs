213ca4178cc1d70c5e3f135c6165699cda6e32fb
Tamas Balog <picimako@gmail.com>
2016-06-13 20:16:52 +0200

    Update comments. Minor readability related changes.

--- Sudoku/Generation/GeneratorUtil.cs
+++ Sudoku/Generation/GeneratorUtil.cs
@@ -12,11 +12,11 @@ namespace Sudoku.Generate
         private int difficulty, killerDifficulty;
         private int[][,] solution;
 
-        //Ebben a szótárban tárolom azokat az Pair listákat, amelyek azon táblákhoz tartoznak melyben a hátramaradt 4 cella négyzet/téglalap alakú
-        //kulcsként tárolom a tábla számát
+        /* Stores Cells which belong to number tables where the left 4 cells are rectangular.
+         * The key is the number of the number table.*/
         private Dictionary<int, List<Cell>> rectangularCells;
 
-        //a törölt értékű cellák indexeit és törlés előtti értékét tárolja
+        //Key: a removed cell. Value: the value of the cell before the removal.
         private Dictionary<Cell, int> removedCellsAndValuesBeforeRemoval;
 
         #endregion
@@ -82,27 +82,28 @@ namespace Sudoku.Generate
             SetValueOfFilledCell(numToFill, cell.Row, cell.Col, kellSzamtombKitolt);
         }
 
-        /// <summary> Beírja r-t a megfelelő táblákba, és beállítja a foglalt cellákat </summary>
-        /// <param name="numToFill"> A beírandó szám </param>
-        /// <param name="row"> A kitöltött cella sorindexe </param>
-        /// <param name="col"> A kitöltött cella oszlopindexe </param>
-        /// <param name="kellSzamtombKitolt"> Megadja, hogy el kell-e végezni a foglalt cellák beállítását tombok[r]-ben </param>
-        public void SetValueOfFilledCell(int numToFill, int row, int col, bool kellSzamtombKitolt)
+        /// <summary> Fills in r into the proper tables and sets the occupied cells.</summary>
+        /// <param name="numToFill">The number to fill in.</param>
+        /// <param name="row">The row index of the filled in cell.</param>
+        /// <param name="col">The column index of the filled in cell.</param>
+        /// <param name="isNumberTableFillingNeeded">Whether occupied cells should be set in se.Exercise[numToFill]</param>
+        public void SetValueOfFilledCell(int numToFill, int row, int col, bool isNumberTableFillingNeeded)
         {
-            se.Exercise[0][row, col] = se.Exercise[numToFill][row, col] = numToFill;
+            se.Exercise[0][row, col] = numToFill;
+            se.Exercise[numToFill][row, col] = numToFill;
 
             for (int num = 1; num <= 9; num++)
             {
-                //Ha nem tombok[r]-be akarok írni, akkor tombok[k]-ba írok -1-et az előbb kitöltött cella indexeivel megegyező cellába
+                //Ha nem tombok[r]-be akarok írni, akkor se.Exercise[num]-ba írok -1-et az előbb kitöltött cella indexeivel megegyező cellába
                 if (num != numToFill)
                     se.Exercise[num][row, col] = se.OCCUPIED;
             }
 
-            /* Ha el kell végezni, akkor elvégzem tombok[r]-ben a foglalt cellák beállítását
+            /* Ha el kell végezni, akkor elvégzem se.Exercise[numToFill]-ben a foglalt cellák beállítását
              * Azért adom meg ezt a paramétert, mert a kitöltés során lehet olyan eset, amikor az egész r tömbben már csak egyetlen egy
              * üres cella van, ekkor csak be kell írni a számot, de ebben a tömbben már felesleges a szükséges cellákat -1-re (foglaltra állítani)
              * mert már minden cella r vagy -1 értékű*/
-            if (kellSzamtombKitolt)
+            if (isNumberTableFillingNeeded)
                 se.Ctrl.MakeHousesOccupied(numToFill, row, col);
         }
 
