0c6e40a2fe81a050e1d0e9d46fb82cdea01b4049
Tamas Balog <picimako@gmail.com>
2016-06-14 21:58:29 +0200

    Update comment. Remove duplicate method call in ExerciseGeneratorInitializer

--- Sudoku/Generation/ExerciseGeneratorInitializer.cs
+++ Sudoku/Generation/ExerciseGeneratorInitializer.cs
@@ -6,11 +6,13 @@ namespace Sudoku.Generate
     {
         public ExerciseGeneratorInitializer() { }
 
-        /// <summary> Decides whether it needs to generate the exercise automatically or the user need to read it from
-        /// file, and then acts so.</summary>
+        /// <summary>
+        /// Decides whether it needs to generate the exercise automatically or the user need to read it from
+        /// file, and then acts so.
+        /// </summary>
         /// <param name="difficulty">The difficulty of the exercise.</param>
         /// <param name="killerDifficulty">The difficulty of the exercise if it is a Killer one.</param>
-        /// <returns>False in case of an error, otherwise true.</returns>
+        /// <returns>False in case of an error during exercise read from file, otherwise true.</returns>
         public bool GenerateExercise(int difficulty, int killerDifficulty)
         {
             SudokuExercise se = SudokuExercise.get;
@@ -18,9 +20,9 @@ namespace Sudoku.Generate
             if (!se.IsExerciseGenerated)
             {
                 se.SetControllerForCurrentExerciseType();
+                se.InitExercise();
                 if (!se.IsExerciseKiller)
                 {
-                    se.InitExercise();
                     if (!ExerciseReader.ReadSudoku(se.ExerciseFilePath))
                         return false;
 
@@ -28,7 +30,6 @@ namespace Sudoku.Generate
                 }
                 else
                 {
-                    se.InitExercise();
                     se.InitKillerExercise();
                     if (!ExerciseReader.ReadKillerSudoku(se.ExerciseFilePath))
                         return false;
